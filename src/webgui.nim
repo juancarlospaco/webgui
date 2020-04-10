ea71dac0ee9ea5712f496c1e2b07fe5fdb34d358
Juan Carlos <JuanCarlospaco@gmail.com>
2020-04-09 21:03:58 -0300

    More templates

--- src/webgui.nim
+++ src/webgui.nim
@@ -336,9 +336,9 @@ template addElement*(w: Webview; id, htmlTag: string, position = beforeEnd): str
   assert id.len > 0, "ID must not be empty string, must have an ID"
   "document.querySelector('" & id & "').insertAdjacentElement('" & $position & "',document.createElement('" & htmlTag & "'));"
 
-template currentHtmlPath*(filename: static[string] = "index.html"): static[string] =
-  ## Alias for `static( currentSourcePath().splitPath.head / "index.html" )` for URL of `index.html`
-  $static(currentSourcePath().splitPath.head / "index.html")
+template currentHtmlPath*(filename: static[string] = "index.html"): string =
+  ## Alias for `currentSourcePath().splitPath.head / "index.html"` for URL of `index.html`
+  currentSourcePath().splitPath.head / "index.html"
 
 proc bindProc*[P, R](w: Webview; scope, name: string; p: (proc(param: P): R)) {.used.} =
   ## Do NOT use directly, see `bindProcs` macro.
