b4b551aabc08950f7a178c5e17c6cba16f2225b3
bung87 <crc32@qq.com>
2020-04-21 06:13:17 +0800

    fix dataURiHtmlHeader case according to https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs

--- src/webgui.nim
+++ src/webgui.nim
@@ -702,7 +702,7 @@ proc newWebView*(path: static[string] = ""; title = ""; width: Positive = 640; h
     when path.endsWith".html": fileLocalHeader & path
     elif path.endsWith".js" or path.endsWith".nim":
       dataUriHtmlHeader & encodeUrl "<!DOCTYPE html><html><head><meta content='width=device-width,initial-scale=1' name=viewport></head><body id=body ><div id=ROOT ><div></body></html>"  # Copied from Karax
-    elif path.len == 0: dataUriHtmlHeader & staticRead"demo.html"
+    elif path.len == 0: dataUriHtmlHeader & encodeUrl staticRead"demo.html"
     else: dataUriHtmlHeader & encodeUrl path.strip
   result = webView(title, url, width, height, resizable, debug, callback)
   when skipTaskbar: result.setSkipTaskbar(skipTaskbar)
